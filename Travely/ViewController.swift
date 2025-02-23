0e85c2b6863460995968088a2251ea65805677e2
athehmd <36544534+athehmd@users.noreply.github.com>
2025-02-23 14:48:28 -0800

    Add files via upload

---
+++ Travely/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Travely
+//
+//  Created by Ather Ahmed on 2/23/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

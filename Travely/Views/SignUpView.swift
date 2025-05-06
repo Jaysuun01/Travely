7a2b90e5f63530a3409b943def95d0b967d0c905
Phat <phat.chauu26@gmail.com>
2025-05-05 23:59:15 -0700

    Phat - Add the inputView for Input text field Modified loginView for normal login with name-password created SignUp
    - retested, but free to modify

---
+++ Travely/Views/SignUpView.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpView.swift
+//  Travely
+//
+//  Created by Phat is here on 5/1/25.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpView()
+}
